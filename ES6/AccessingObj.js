bc9c3b1226a4bde701ab3d401fed1d5eb3b5fe13
Aman Tiwari <36691571+amantiwari8861@users.noreply.github.com>
2022-11-21 00:31:34 +0530

    date object

--- ES6/AccessingObj.js
+++ ES6/AccessingObj.js
@@ -1,1 +1,11 @@
-AccessingObj.html
+// let user = {
+//     name: "Rexha",
+//     age: 24,
+//     isConfirmed: true
+// };
+// for (let key in user) {
+//     // keys 
+//     // console.log(key);// name, age, isConfirmed 
+//     // values for the keys 
+//     console.log(key +" : "+user[key]);
+// }
