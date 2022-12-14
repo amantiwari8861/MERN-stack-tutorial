94f5b17918eba12de05c7206c00f88c1cbd1e33b
Aman Tiwari <36691571+amantiwari8861@users.noreply.github.com>
2022-12-15 00:38:36 +0530

    modular programming

---
+++ ES6/course2.js
@@ -0,0 +1,7 @@
+import crs, { courseName } from "./course1.js";
+console.log(courseName);
+console.log(crs.getCourseName());
+crs.setCourseName("ES6");
+console.log(crs.getCourseName());
+
+// node --experimental-modules filename.mjs
